a3bbcb07397f7e879153ea7ec2f8e3d55f5f51ab
kf <asano.kenich@gmail.com>
2018-06-18 12:45:59 +0900

    first commit

---
+++ CSBombmanClientFu/Com.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSBombmanClientFu
+{
+    class Com
+    {
+        private int myId = -1;
+
+        public Com() { }
+
+        public void Run()
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // player name
+            Console.WriteLine("ふ", Console.OutputEncoding.CodePage);
+
+            // get player id
+            SetId(Console.ReadLine());
+
+            while (true)
+            {
+                string data = Console.ReadLine();
+
+                try
+                {
+                }
+            }
+        }
+
+        private void SetId(string idStr)
+        {
+            if (int.TryParse(idStr, out int x))
+            {
+                myId = x;
+            }
+        }
+    }
+}
---
+++ CSBombmanClientFu/State.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSBombmanClientFu
+{
+    class State
+    {
+        public int turn { get; set; }
+
+    }
+}
